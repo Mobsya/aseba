3e4f0377a03f11bd4771cf016e6d8b0ce98cb3e2
Corentin Jabot <corentin.jabot@mobsya.org>
2018-05-02 09:02:06 +0200

    Direct logs to logcat on android

--- aseba/clients/vpl2/src/app/main.cpp
+++ aseba/clients/vpl2/src/app/main.cpp
@@ -3,13 +3,50 @@
 #include <QTranslator>
 #include "thymio-vpl2.h"
 #include "liveqmlreloadingengine.h"
+#include <QDebug>
+
+
+const char* const applicationName = "Thymio VPL Mobile Preview";
+
+#ifdef ANDROID  // Set in my myapp.pro file for android builds
+#    include <android/log.h>
+
+void myMessageHandler(QtMsgType type, const QMessageLogContext& context, const QString& msg) {
+    QString report = msg;
+    if(context.file && !QString(context.file).isEmpty()) {
+        report += " in file ";
+        report += QString(context.file);
+        report += " line ";
+        report += QString::number(context.line);
+    }
+    if(context.function && !QString(context.function).isEmpty()) {
+        report += +" function ";
+        report += QString(context.function);
+    }
+    const char* const local = report.toLocal8Bit().constData();
+    switch(type) {
+        case QtDebugMsg: __android_log_write(ANDROID_LOG_DEBUG, applicationName, local); break;
+        case QtInfoMsg: __android_log_write(ANDROID_LOG_INFO, applicationName, local); break;
+        case QtWarningMsg: __android_log_write(ANDROID_LOG_WARN, applicationName, local); break;
+        case QtCriticalMsg: __android_log_write(ANDROID_LOG_ERROR, applicationName, local); break;
+        case QtFatalMsg:
+        default: __android_log_write(ANDROID_LOG_FATAL, applicationName, local); abort();
+    }
+}
+#endif
 
 int main(int argc, char* argv[]) {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QGuiApplication app(argc, argv);
+    app.setApplicationName(applicationName);
+
+#ifdef ANDROID
+    qInstallMessageHandler(myMessageHandler);
+#endif
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     app.setOrganizationName("Mobsya");
     app.setOrganizationDomain("mobsya.org");
-    app.setApplicationName("Thymio VPL Mobile Preview");
 
     QTranslator qtTranslator;
     qtTranslator.load("thymio-vpl2_" + QLocale::system().name(), ":/thymio-vpl2/translations/");
