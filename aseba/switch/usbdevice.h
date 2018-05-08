1538bf8fba0c23f42a166fd3de36094db5d41a4b
Corentin Jabot <corentin.jabot@mobsya.org>
2018-05-08 16:48:35 +0200

    Reading from an usb device needs to be buffered

--- aseba/switch/usbdevice.h
+++ aseba/switch/usbdevice.h
@@ -38,6 +38,42 @@ public:
     void shutdown() override;
 
 
+    class buffer {
+    public:
+        buffer() : s(0) {}
+        void reserve(std::size_t size, std::size_t buffer_size) {
+            buffer_size = std::max(buffer_size, std::size_t{1});
+            v.resize(((s + size + buffer_size - 1) / buffer_size) * buffer_size);
+        }
+
+        void read(const boost::asio::mutable_buffer& b) {
+            const auto n = b.size();
+            std::copy(v.data(), v.data() + n, static_cast<uint8_t*>(b.data()));
+            std::vector<uint8_t>(v.begin() + n, v.end()).swap(v);
+        }
+
+        uint8_t* write_begin() {
+            return v.data() + s;
+        }
+
+        std::size_t write_capacity() const {
+            return v.size() - s;
+        }
+
+        std::size_t size() const {
+            return s;
+        }
+
+        void commit(std::size_t n) {
+            s += n;
+        }
+
+    private:
+        std::vector<uint8_t> v;
+        std::size_t s;
+    };
+
+
     struct implementation_type {
         libusb_device* device = nullptr;
         libusb_device_handle* handle = nullptr;
@@ -47,6 +83,7 @@ public:
         uint8_t in_address = 0;
         std::size_t read_size = 0;
         std::size_t write_size = 0;
+        buffer read_buffer;
     };
     void construct(implementation_type&);
     void destroy(implementation_type&);
@@ -59,15 +96,15 @@ public:
     tl::expected<void, boost::system::error_code> open(implementation_type& impl);
 
 
-    template <typename ConstBufferSequence, typename WriteHandler>
+    /*template <typename ConstBufferSequence, typename WriteHandler>
     void async_write_some(implementation_type& impl, const ConstBufferSequence& buffers, WriteHandler&& handler) {
-        async_transfer_some(impl, impl.out_address, buffers, std::forward<WriteHandler>(handler));
+        async_transfer_some<WriteDirection>(impl, buffers, std::forward<WriteHandler>(handler));
     }
 
     template <typename MutableBufferSequence, typename ReadHandler>
     void async_read_some(implementation_type& impl, const MutableBufferSequence& buffers, ReadHandler&& handler) {
-        async_transfer_some(impl, impl.in_address, buffers, std::forward<ReadHandler>(handler));
-    }
+        async_transfer_some<ReadDirection>(impl, buffers, std::forward<ReadHandler>(handler));
+    }*/
 
     template <typename ConstBufferSequence>
     tl::expected<std::size_t, boost::system::error_code> write_some(implementation_type& impl,
@@ -88,6 +125,22 @@ public:
     void set_data_terminal_ready(implementation_type& impl, bool dtr);
 
 private:
+    friend class usb_device;
+    template <typename MutableBufferSequence>
+    typename MutableBufferSequence::const_iterator
+    read_from_buffer(implementation_type& impl, const typename MutableBufferSequence::const_iterator& begin,
+                     const typename MutableBufferSequence::const_iterator& end, std::size_t& read) {
+        usb_device_service::buffer& read_buffer = impl.read_buffer;
+        for(auto it = begin; it != end; ++it) {
+            if(read_buffer.size() < it->size())
+                return it;
+            read_buffer.read(*it);
+            read += it->size();
+        }
+        return end;
+    }
+
+
     details::usb_context::ptr m_context;
     bool send_control_transfer(implementation_type& impl);
     bool send_encoding(implementation_type& impl);
@@ -95,6 +148,9 @@ private:
 
 
 class usb_device : public boost::asio::basic_io_object<usb_device_service> {
+    using ReadDirection = std::true_type;
+    using WriteDirection = std::false_type;
+
 public:
     using baud_rate = usb_device_service::baud_rate;
     using data_bits = usb_device_service::data_bits;
@@ -121,8 +177,8 @@ public:
                       "ConstBufferSequence requirements not met");
 
         boost::asio::async_completion<WriteHandler, void(boost::system::error_code, std::size_t)> init(handler);
-        this->async_transfer_some(this->get_implementation().out_address, buffers,
-                                  std::forward<WriteHandler>(init.completion_handler));
+        this->async_transfer_some<ConstBufferSequence, WriteHandler, WriteDirection>(
+            buffers, std::forward<WriteHandler>(init.completion_handler));
         return init.result.get();
     }
 
@@ -134,8 +190,8 @@ public:
                       "MutableBufferSequence requirements not met");
 
         boost::asio::async_completion<ReadHandler, void(boost::system::error_code, std::size_t)> init(handler);
-        this->async_transfer_some(this->get_implementation().in_address, buffers,
-                                  std::forward<ReadHandler>(init.completion_handler));
+        this->async_transfer_some<MutableBufferSequence, ReadHandler, ReadDirection>(
+            buffers, std::forward<ReadHandler>(init.completion_handler));
         return init.result.get();
     }
 
@@ -192,8 +248,8 @@ public:
     }
 
 private:
-    template <typename BufferSequence, typename CompletionHandler>
-    void async_transfer_some(uint8_t address, const BufferSequence& buffers, CompletionHandler&& handler);
+    template <typename BufferSequence, typename CompletionHandler, typename TransferDirection>
+    void async_transfer_some(const BufferSequence& buffers, CompletionHandler&& handler);
 };
 
 template <typename ConstBufferSequence>
@@ -220,13 +276,20 @@ usb_device_service::write_some(implementation_type& impl, const ConstBufferSeque
 template <typename MutableBufferSequence>
 tl::expected<std::size_t, boost::system::error_code>
 usb_device_service::read_some(implementation_type& impl, const MutableBufferSequence& buffers) {
+
     std::size_t total_read = 0;
-    for(const boost::asio::mutable_buffer& b : boost::beast::detail::buffers_range(buffers)) {
+    auto it = read_from_buffer(impl, boost::asio::buffer_sequence_begin(buffers),
+                               boost::asio::buffer_sequence_end(buffers), total_read);
+    for(; it != boost::asio::buffer_sequence_end(buffers); ++it) {
+        impl.read_buffer.reserve(it->size(), impl.read_size);
         int read = 0;
-        auto err = libusb_bulk_transfer(impl.handle, impl.in_address, static_cast<unsigned char*>(b.data()), b.size(),
-                                        &read, 10);
+        auto err = libusb_bulk_transfer(impl.handle, impl.in_address,
+                                        static_cast<unsigned char*>(impl.read_buffer.write_begin()),
+                                        impl.read_buffer.write_capacity(), &read, 10);
         if(err == LIBUSB_SUCCESS || err == LIBUSB_ERROR_TIMEOUT) {
             total_read += read;
+            impl.read_buffer.commit(read);
+            impl.read_buffer.read(*it);
             if(err == LIBUSB_ERROR_TIMEOUT)
                 break;
         } else {
@@ -235,28 +298,56 @@ usb_device_service::read_some(implementation_type& impl, const MutableBufferSequ
     }
     return total_read;
 }
+namespace detail {
+    template <typename BufferSequence, typename Callback, typename TransferDirection>
+    void prepare_transfer(usb_device_service::implementation_type& impl, libusb_transfer* transfer,
+                          const BufferSequence& buffers, std::size_t& index, Callback&& cb, void* user_data) {
+        uint8_t address;
+        uint8_t* buffer;
+        const auto it = boost::asio::buffer_sequence_begin(buffers) + index;
+        std::size_t buffer_size = 0;
+        if constexpr(TransferDirection::value) {
+            auto to_read = std::accumulate(it, boost::asio::buffer_sequence_end(buffers), std::size_t{0},
+                                           [](const auto& a, const auto& b) { return a + b.size(); });
 
-template <typename BufferSequence, typename CompletionHandler>
-void usb_device::async_transfer_some(uint8_t address, const BufferSequence& buffers, CompletionHandler&& handler) {
+            impl.read_buffer.reserve(to_read, impl.read_size);
+            buffer_size = impl.read_buffer.write_capacity();
+            buffer = impl.read_buffer.write_begin();
+            address = impl.in_address;
+
+        } else {
+            buffer_size = it->size();
+            buffer = (uint8_t*)(it->data());
+            address = impl.out_address;
+        }
+        libusb_fill_bulk_transfer(transfer, impl.handle, address, buffer, buffer_size, std::forward<Callback>(cb),
+                                  user_data, 0);
+    }
+
+}  // namespace detail
+
+template <typename BufferSequence, typename CompletionHandler, typename TransferDirection>
+void usb_device::async_transfer_some(const BufferSequence& buffers, CompletionHandler&& handler) {
+
 
     struct transfer_data;
-
     using data_allocator_t = typename std::allocator_traits<decltype(
         boost::asio::get_associated_allocator(handler))>::template rebind_alloc<transfer_data>;
 
     struct transfer_data {
         BufferSequence seq;
         usb_device& device;
-        typename BufferSequence::const_iterator it;
+        std::size_t idx;
         std::size_t total_transfered;
-        CompletionHandler&& handler;
+        CompletionHandler handler;
         data_allocator_t alloc;
 
-        transfer_data(BufferSequence seq, usb_device& device, CompletionHandler&& handler)
+        transfer_data(BufferSequence seq, usb_device& device, CompletionHandler&& handler, std::size_t idx = 0,
+                      std::size_t total_transfered = 0)
             : seq(seq)
             , device(device)
-            , it(boost::asio::buffer_sequence_begin(seq))
-            , total_transfered(0)
+            , idx(idx)
+            , total_transfered(total_transfered)
             , handler(std::forward<CompletionHandler>(handler)) {}
 
         static void destroy(transfer_data* d) {
@@ -268,39 +359,64 @@ void usb_device::async_transfer_some(uint8_t address, const BufferSequence& buff
             return std::unique_ptr<transfer_data, decltype(&transfer_data::destroy)>(d, &transfer_data::destroy);
         }
         static auto allocate(data_allocator_t&& allocator, const BufferSequence& buffers, usb_device& device,
-                             CompletionHandler&& handler) {
+                             CompletionHandler&& handler, std::size_t idx = 0, std::size_t total_transfered = 0) {
             transfer_data* data = allocator.allocate(1);
-            std::allocator_traits<data_allocator_t>::construct(allocator, data, buffers, device,
-                                                               std::forward<CompletionHandler>(handler));
+            std::allocator_traits<data_allocator_t>::construct(
+                allocator, data, buffers, device, std::forward<CompletionHandler>(handler), idx, total_transfered);
             data->alloc = std::move(allocator);
             return std::unique_ptr<transfer_data, decltype(&transfer_data::destroy)>(data, &transfer_data::destroy);
         }
     };
 
-    const auto& impl = this->get_implementation();
+    auto& impl = this->get_implementation();
+    auto start_it = boost::asio::buffer_sequence_begin(buffers);
+    std::size_t total_transfered = 0;
+
+    if constexpr(TransferDirection::value) {
+        start_it = get_service().read_from_buffer<BufferSequence>(
+            impl, start_it, boost::asio::buffer_sequence_end(buffers), total_transfered);
+        if(start_it == boost::asio::buffer_sequence_end(buffers)) {
+            boost::asio::post(get_executor(),
+                              boost::beast::bind_handler(std::forward<CompletionHandler>(handler),
+                                                         boost::system::error_code{}, total_transfered));
+            return;
+        }
+    }
 
     using data_allocator_t = typename std::allocator_traits<decltype(
         boost::asio::get_associated_allocator(handler))>::template rebind_alloc<transfer_data>;
     data_allocator_t alloc(boost::asio::get_associated_allocator(handler));
-    auto data = transfer_data::allocate(std::move(alloc), buffers, *this, std::forward<CompletionHandler>(handler));
-
-    auto transfer = libusb_alloc_transfer(0);
+    auto data =
+        transfer_data::allocate(std::move(alloc), buffers, *this, std::forward<CompletionHandler>(handler),
+                                std::distance(boost::asio::buffer_sequence_begin(buffers), start_it), total_transfered);
 
     static auto cb = [](libusb_transfer* transfer) {
         auto d = transfer_data::unique_ptr(static_cast<transfer_data*>(transfer->user_data));
-
-        d->total_transfered += transfer->actual_length;
-
-
         // Successful transfer and full buffer => read some more in the next buffer
-        if(transfer->status == LIBUSB_TRANSFER_COMPLETED && transfer->actual_length == transfer->length &&
-           d->it++ != std::end(d->seq)) {
-            transfer->actual_length = 0;
-            transfer->buffer = (decltype(transfer->buffer))d->it->data();
-            transfer->length = d->it->size();
-            libusb_submit_transfer(transfer);
-            d.release();
-            return;
+        if(transfer->status == LIBUSB_TRANSFER_COMPLETED) {
+            auto it = boost::asio::buffer_sequence_begin(d->seq) + d->idx;
+            auto& impl = d->device.get_implementation();
+            if constexpr(TransferDirection::value) {
+                impl.read_buffer.commit(transfer->actual_length);
+                usb_device_service& service = d->device.get_service();
+                it = service.read_from_buffer<BufferSequence>(impl, it, boost::asio::buffer_sequence_end(d->seq),
+                                                              d->total_transfered);
+                d->idx = std::distance(boost::asio::buffer_sequence_begin(d->seq), it);
+            } else {
+                d->total_transfered = transfer->actual_length;
+                it++;
+                d->idx++;
+            }
+
+
+            if(it != boost::asio::buffer_sequence_end(d->seq)) {
+                detail::prepare_transfer<BufferSequence, std::remove_reference_t<decltype(transfer->callback)>,
+                                         TransferDirection>(
+                    impl, transfer, d->seq, d->idx, (libusb_transfer_cb_fn)transfer->callback, transfer->user_data);
+                libusb_submit_transfer(transfer);
+                d.release();
+                return;
+            }
         }
         boost::system::error_code ec;
         if(transfer->status != LIBUSB_TRANSFER_COMPLETED) {
@@ -320,13 +436,16 @@ void usb_device::async_transfer_some(uint8_t address, const BufferSequence& buff
                           boost::beast::bind_handler(std::forward<CompletionHandler>(handler), ec, total_transfered));
     };
 
-    libusb_fill_bulk_transfer(transfer, impl.handle, address, decltype(transfer->buffer)(data->it->data()),
-                              data->it->size(), cb, data.get(), 0);
+
+    auto transfer = libusb_alloc_transfer(0);
+    detail::prepare_transfer<BufferSequence, std::remove_reference_t<decltype(cb)>, TransferDirection>(
+        impl, transfer, data->seq, data->idx, std::move(cb), data.get());
     auto r = libusb_submit_transfer(transfer);
     if(r != LIBUSB_SUCCESS) {
         libusb_free_transfer(transfer);
+        auto handler = std::move(data->handler);
         boost::asio::post(
-            boost::asio::get_associated_executor(handler, this->get_executor()),
+            data->device.get_executor(),
             boost::beast::bind_handler(std::forward<CompletionHandler>(handler), usb::make_error_code(r), 0));
     } else {
         data.release();
