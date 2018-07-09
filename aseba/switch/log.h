aec6e8748d2ee7f825b8bf96eed20432eecf6ce4
Corentin Jabot <corentin.jabot@mobsya.org>
2018-07-09 12:00:47 +0200

    Do not log full paths

--- aseba/switch/log.h
+++ aseba/switch/log.h
@@ -5,13 +5,17 @@
 
 namespace mobsya {
 extern std::shared_ptr<spdlog::logger> log;
-}
+extern std::string log_filename(const char* path);
+
+}  // namespace mobsya
 #define _mobsya_CONCAT2(A, B) A##B
 #define _mobsya_CONCAT(A, B) _mobsya_CONCAT2(A, B)
 #define _mobsya_STR2(x) #x
 #define _mobsya_STR(x) _mobsya_STR2(x)
 
-#define _mobsya_Log(level, ...) mobsya::log->log(level, __FILE__ "@L" _mobsya_STR(__LINE__) ":\t" __VA_ARGS__)
+#define _mobsya_Log(level, message, ...)                                                                           \
+    mobsya::log->log(level, fmt::format("{}@L{}:\t{}", mobsya::log_filename(__FILE__), __LINE__, message).c_str(), \
+                     ##__VA_ARGS__)
 #define mLogTrace(...) _mobsya_Log(spdlog::level::trace, __VA_ARGS__)
 #define mLogDebug(...) _mobsya_Log(spdlog::level::debug, __VA_ARGS__)
 #define mLogInfo(...) _mobsya_Log(spdlog::level::info, __VA_ARGS__)
