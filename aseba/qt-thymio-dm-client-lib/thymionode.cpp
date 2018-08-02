0cff7f1716053fccd36072c4d39b5235eeb58d11
Corentin Jabot <corentin.jabot@mobsya.org>
2018-08-02 17:39:21 +0200

    Initial work on clientside Qt lib

---
+++ aseba/qt-thymio-dm-client-lib/thymionode.cpp
@@ -0,0 +1,36 @@
+#include "thymionode.h"
+
+
+namespace mobsya {
+
+ThymioNode::ThymioNode(std::shared_ptr<ThymioDeviceManagerClientEndpoint> endpoint, const QUuid& uuid,
+                       const QString& name)
+    : m_endpoint(endpoint), m_uuid(uuid), m_name(name), m_status(Status::disconnected) {}
+
+
+QUuid ThymioNode::uuid() const {
+    return m_uuid;
+}
+
+QString ThymioNode::name() const {
+    return m_name;
+}
+
+ThymioNode::Status ThymioNode::status() const {
+    return m_status;
+}
+
+void ThymioNode::setName(const QString& name) {
+    if(m_name != name) {
+        m_name = name;
+        Q_EMIT nameChanged();
+    }
+}
+void ThymioNode::setStatus(const Status& status) {
+    if(m_status != status) {
+        m_status = status;
+        Q_EMIT statusChanged();
+    }
+}
+
+}  // namespace mobsya
