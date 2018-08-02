391e2a7dc82fc550617737d60e694e2e02516dec
Corentin Jabot <corentin.jabot@mobsya.org>
2018-08-02 17:36:49 +0200

    Move fb_message_ptr to flatbuffers/

---
+++ aseba/flatbuffers/fb_message_ptr.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <aseba/flatbuffers/thymio_generated.h>
+
+namespace mobsya {
+
+class fb_message_ptr {
+public:
+    fb_message_ptr() : m_msg(nullptr) {}
+    fb_message_ptr(const fb_message_ptr&) = delete;
+    fb_message_ptr(fb_message_ptr&& other) : m_data(std::move(other.m_data)), m_msg(other.m_msg) {
+        other.m_msg = nullptr;
+    }
+    fb_message_ptr(std::vector<uint8_t>&& data) : m_data(std::move(data)) {
+        m_msg = fb::GetMessage(m_data.data());
+    }
+
+    operator bool() const {
+        return m_msg;
+    }
+
+    const fb::Message* operator->() const {
+        return m_msg;
+    }
+
+    const fb::Message& operator*() const {
+        return *m_msg;
+    }
+
+    fb::AnyMessage message_type() const {
+        return m_msg->message_type();
+    }
+
+    template <typename T>
+    const T* as() const {
+        return m_msg->message_as<T>();
+    }
+
+private:
+    std::vector<uint8_t> m_data;
+    const fb::Message* m_msg;
+};
+
+}  // namespace mobsya
--- aseba/switch/flatbuffers_message_reader.h
+++ aseba/switch/flatbuffers_message_reader.h
@@ -2,50 +2,13 @@
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 #include <aseba/flatbuffers/thymio_generated.h>
+#include <aseba/flatbuffers/fb_message_ptr.h>
 #include "log.h"
 
 namespace mobsya {
 
 template <class AsyncReadStream, class Handler>
 class read_flatbuffers_message_op;
-
-
-class fb_message_ptr {
-public:
-    fb_message_ptr() : m_msg(nullptr) {}
-    fb_message_ptr(const fb_message_ptr&) = delete;
-    fb_message_ptr(fb_message_ptr&& other) : m_data(std::move(other.m_data)), m_msg(other.m_msg) {
-        other.m_msg = nullptr;
-    }
-    fb_message_ptr(std::vector<uint8_t>&& data) : m_data(std::move(data)) {
-        m_msg = fb::GetMessage(m_data.data());
-    }
-
-    operator bool() const {
-        return m_msg;
-    }
-
-    const fb::Message* operator->() const {
-        return m_msg;
-    }
-
-    const fb::Message& operator*() const {
-        return *m_msg;
-    }
-
-    fb::AnyMessage message_type() const {
-        return m_msg->message_type();
-    }
-
-    template <typename T>
-    const T* as() const {
-        return m_msg->message_as<T>();
-    }
-
-private:
-    std::vector<uint8_t> m_data;
-    const fb::Message* m_msg;
-};
 
 using read_flatbuffers_message_op_cb_t = void(boost::system::error_code, fb_message_ptr);
 
