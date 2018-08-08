f7ace419b627fb5f05056fe4a37232df87349820
Corentin Jabot <corentin.jabot@mobsya.org>
2018-08-08 11:41:20 +0200

    More work on qt client lib & launcher

--- aseba/launcher/src/main.cpp
+++ aseba/launcher/src/main.cpp
@@ -3,16 +3,25 @@
 #include <QDir>
 #include <QDebug>
 #include <QQmlApplicationEngine>
+#include <aseba/qt-thymio-dm-client-lib/thymio-api.h>
 
 int main(int argc, char** argv) {
 
     QApplication app(argc, argv);
+    mobsya::register_qml_types();
+
     for(QString& file : QDir(":/fonts").entryList(QDir::Files)) {
         QString path = ":/fonts/" + file;
         qDebug() << path;
         QFontDatabase::addApplicationFont(path);
     }
+
+    mobsya::ThymioDeviceManagerClient client;
+    mobsya::ThymioDevicesModel model(client);
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("thymios", &model);
+
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
     return app.exec();
 }
