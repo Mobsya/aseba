ea11b7b0b8faba756b83235fcbb61e78f18cc705
cor3ntin <corentinjabot@gmail.com>
2018-08-14 13:32:51 +0200

    usb_device_identifier will be useful for serial too

--- aseba/switch/usbcontext.h
+++ aseba/switch/usbcontext.h
@@ -6,17 +6,9 @@
 #include <vector>
 #include <algorithm>
 #include "log.h"
+#include "usb_utils.h"
 
 namespace mobsya {
-
-struct usb_device_identifier {
-    uint16_t vendor_id;
-    uint16_t product_id;
-};
-
-inline bool operator==(const usb_device_identifier& a, const usb_device_identifier& b) {
-    return a.vendor_id == b.vendor_id && a.product_id == b.product_id;
-}
 
 namespace details {
     class usb_context {
