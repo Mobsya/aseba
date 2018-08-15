cd70565b7dee5841c66a304466f8272dbbb50ad3
Corentin Jabot <corentin.jabot@mobsya.org>
2018-08-15 16:59:28 +0200

    Handle setting a new name in the model

--- aseba/qt-thymio-dm-client-lib/thymiodevicesmodel.h
+++ aseba/qt-thymio-dm-client-lib/thymiodevicesmodel.h
@@ -15,6 +15,7 @@ public:
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
     QHash<int, QByteArray> roleNames() const override;
     bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole) override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
 
 private Q_SLOTS:
     void updateModel();
