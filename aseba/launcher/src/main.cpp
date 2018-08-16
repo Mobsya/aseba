cb97c8bca8b5211dbf5c1cfe25867a19564e5bc5
Corentin Jabot <corentin.jabot@mobsya.org>
2018-08-16 15:35:11 +0200

    enable qml debugging support

--- aseba/launcher/src/main.cpp
+++ aseba/launcher/src/main.cpp
@@ -3,10 +3,17 @@
 #include <QDir>
 #include <QDebug>
 #include <QQmlApplicationEngine>
+#include <QSurfaceFormat>
+#ifdef QT_QML_DEBUG
+#    include <QQmlDebuggingEnabler>
+#endif
 #include <aseba/qt-thymio-dm-client-lib/thymio-api.h>
 
 int main(int argc, char** argv) {
 
+#ifdef QT_QML_DEBUG
+    QQmlDebuggingEnabler enabler;
+#endif
     QApplication app(argc, argv);
     mobsya::register_qml_types();
 
