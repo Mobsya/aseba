8038a43b75b66618b8c90ec52dfe28939f012836
Corentin Jabot <corentin.jabot@mobsya.org>
2018-08-23 15:23:16 +0200

    [Qt API] Correctly read all incomming messages

--- aseba/qt-thymio-dm-client-lib/thymiodevicemanagerclientendpoint.cpp
+++ aseba/qt-thymio-dm-client-lib/thymiodevicemanagerclientendpoint.cpp
@@ -12,21 +12,23 @@ ThymioDeviceManagerClientEndpoint::ThymioDeviceManagerClientEndpoint(QTcpSocket*
 }
 
 void ThymioDeviceManagerClientEndpoint::onReadyRead() {
-    if(m_message_size == 0) {
-        if(m_socket->bytesAvailable() < 4)
+    while(true) {
+        if(m_message_size == 0) {
+            if(m_socket->bytesAvailable() < 4)
+                return;
+            char data[4];
+            m_socket->read(data, 4);
+            m_message_size = *reinterpret_cast<quint32*>(data);
+        }
+        if(m_socket->bytesAvailable() < m_message_size)
             return;
-        char data[4];
-        m_socket->read(data, 4);
-        m_message_size = *reinterpret_cast<quint32*>(data);
+        std::vector<uint8_t> data;
+        data.resize(m_message_size);
+        auto s = m_socket->read(reinterpret_cast<char*>(data.data()), m_message_size);
+        Q_ASSERT(s == m_message_size);
+        m_message_size = 0;
+        Q_EMIT onMessage(fb_message_ptr(std::move(data)));
     }
-    if(m_socket->bytesAvailable() < m_message_size)
-        return;
-    std::vector<uint8_t> data;
-    data.resize(m_message_size);
-    auto s = m_socket->read(reinterpret_cast<char*>(data.data()), m_message_size);
-    Q_ASSERT(s == m_message_size);
-    m_message_size = 0;
-    Q_EMIT onMessage(fb_message_ptr(std::move(data)));
 }
 
 
