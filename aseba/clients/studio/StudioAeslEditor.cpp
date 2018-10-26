8d0e81427c50ae4393ec8c6a0b26f1b36739f25f
Corentin Jabot <corentin.jabot@mobsya.org>
2018-10-26 09:12:50 +0200

    [Studio] Reenable completion

--- aseba/clients/studio/StudioAeslEditor.cpp
+++ aseba/clients/studio/StudioAeslEditor.cpp
@@ -28,7 +28,9 @@ namespace Aseba {
 /** \addtogroup studio */
 /*@{*/
 
-StudioAeslEditor::StudioAeslEditor(const ScriptTab* tab) : tab(tab), dropSourceWidget(nullptr) {}
+StudioAeslEditor::StudioAeslEditor(const ScriptTab* tab) : tab(tab), dropSourceWidget(nullptr) {
+    connect(this, &StudioAeslEditor::textChanged, this, &StudioAeslEditor::handleCompletion);
+}
 
 void StudioAeslEditor::dropEvent(QDropEvent* event) {
     dropSourceWidget = dynamic_cast<QWidget*>(event->source());
@@ -79,6 +81,74 @@ void StudioAeslEditor::insertFromMimeData(const QMimeData* source) {
     cursor.endEditBlock();
 
     this->setTextCursor(cursor);
+}
+
+void StudioAeslEditor::handleCompletion() {
+    // handle completion
+    QTextCursor cursor(textCursor());
+    if(!cursor.atBlockEnd())
+        return;
+
+    // language completion
+    const QString& line(cursor.block().text());
+    QString keyword(line);
+
+    // make sure the string does not have any trailing space
+    int nonWhitespace(0);
+    while((nonWhitespace < keyword.size()) &&
+          ((keyword.at(nonWhitespace) == ' ') || (keyword.at(nonWhitespace) == '\t')))
+        ++nonWhitespace;
+    keyword.remove(0, nonWhitespace);
+
+    if(!keyword.trimmed().isEmpty()) {
+        QString prefix;
+        QString postfix;
+        if(keyword == QLatin1String("if")) {
+            const QString headSpace = line.left(line.indexOf(QLatin1String("if")));
+            prefix = QLatin1String(" ");
+            postfix = " then\n" + headSpace + "\t\n" + headSpace + "end";
+        } else if(keyword == QLatin1String("when")) {
+            const QString headSpace = line.left(line.indexOf(QLatin1String("when")));
+            prefix = QLatin1String(" ");
+            postfix = " do\n" + headSpace + "\t\n" + headSpace + "end";
+        } else if(keyword == QLatin1String("for")) {
+            const QString headSpace = line.left(line.indexOf(QLatin1String("for")));
+            prefix = QLatin1String(" ");
+            postfix = "i in 0:0 do\n" + headSpace + "\t\n" + headSpace + "end";
+        } else if(keyword == QLatin1String("while")) {
+            const QString headSpace = line.left(line.indexOf(QLatin1String("while")));
+            prefix = QLatin1String(" ");
+            postfix = " do\n" + headSpace + "\t\n" + headSpace + "end";
+        } else if((keyword == QLatin1String("else")) && cursor.block().next().isValid()) {
+            const QString tab = QStringLiteral("\t");
+            QString headSpace = line.left(line.indexOf(QLatin1String("else")));
+
+            if(headSpace.size() >= tab.size()) {
+                headSpace = headSpace.left(headSpace.size() - tab.size());
+                if(cursor.block().next().text() == headSpace + "end") {
+                    prefix = "\n" + headSpace + "else";
+                    postfix = "\n" + headSpace + "\t";
+
+                    cursor.select(QTextCursor::BlockUnderCursor);
+                    cursor.removeSelectedText();
+                }
+            }
+        } else if(keyword == QLatin1String("elseif")) {
+            const QString headSpace = line.left(line.indexOf(QLatin1String("elseif")));
+            prefix = QLatin1String(" ");
+            postfix = QLatin1String(" then");
+        }
+
+        if(!prefix.isNull() || !postfix.isNull()) {
+            cursor.beginEditBlock();
+            cursor.insertText(prefix);
+            const int pos = cursor.position();
+            cursor.insertText(postfix);
+            cursor.setPosition(pos);
+            cursor.endEditBlock();
+            setTextCursor(cursor);
+        }
+    }
 }
 
 }  // namespace Aseba
