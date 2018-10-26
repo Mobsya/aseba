274fb652c8e33378404ac45fd039e17146edf276
Corentin Jabot <corentin.jabot@mobsya.org>
2018-10-26 07:40:19 +0200

    [Studio] Improve tab management

--- aseba/clients/studio/NodeTabsManager.cpp
+++ aseba/clients/studio/NodeTabsManager.cpp
@@ -6,28 +6,21 @@ namespace Aseba {
 //////
 
 NodeTabsManager::NodeTabsManager(const mobsya::ThymioDeviceManagerClient& client) : m_client(client) {
-    readSettings();  // read user's preferences
     connect(this, &QTabWidget::currentChanged, this, &NodeTabsManager::tabChanged);
+    connect(this, &QTabWidget::tabCloseRequested, this, &NodeTabsManager::onTabClosed);
 
     connect(&m_client, &mobsya::ThymioDeviceManagerClient::nodeAdded, this, &NodeTabsManager::onNodeAdded);
     connect(&m_client, &mobsya::ThymioDeviceManagerClient::nodeModified, this, &NodeTabsManager::onNodeModified);
     connect(&m_client, &mobsya::ThymioDeviceManagerClient::nodeRemoved, this, &NodeTabsManager::onNodeRemoved);
 }
 
-NodeTabsManager::~NodeTabsManager() {
-    // store user's preferences
-    writeSettings();
-}
+NodeTabsManager::~NodeTabsManager() {}
 
 void NodeTabsManager::addTab(const QUuid& device) {
     if(m_tabs.contains(device))
         return;
     auto tab = new NodeTab();
     const int index = QTabWidget::addTab(tab, device.toString());
-    QPushButton* button = new QPushButton(QIcon(":/images/remove.png"), QLatin1String(""));
-    button->setFlat(true);
-    connect(button, &QAbstractButton::clicked, this, &NodeTabsManager::removeAndDeleteTab);
-    tabBar()->setTabButton(index, QTabBar::RightSide, button);
     m_tabs[device] = tab;
     if(auto thymio = m_client.node(device)) {
         tab->setThymio(thymio);
@@ -38,6 +31,9 @@ void NodeTabsManager::addTab(const QUuid& device) {
 
 
 void NodeTabsManager::onNodeAdded(std::shared_ptr<mobsya::ThymioNode> thymio) {
+    if(thymio->status() == mobsya::ThymioNode::Status::Connected)
+        return;
+
     auto it = m_tabs.find(thymio->uuid());
     if(it != m_tabs.end()) {
         (*it)->setThymio(thymio);
@@ -54,7 +50,7 @@ void NodeTabsManager::onNodeRemoved(std::shared_ptr<mobsya::ThymioNode> thymio) 
     if(it != m_tabs.end()) {
         (*it)->setThymio({});
         auto idx = QTabWidget::indexOf(*it);
-        // QTabWidget::setTabEnabled(idx, false);
+        QTabWidget::setTabEnabled(idx, false);
     }
 }
 
@@ -62,7 +58,8 @@ void NodeTabsManager::onNodeModified(std::shared_ptr<mobsya::ThymioNode> thymio)
     auto it = m_tabs.find(thymio->uuid());
     if(it != m_tabs.end()) {
         auto idx = QTabWidget::indexOf(*it);
-        tabBar()->setTabText(idx, thymio->name());
+        if(thymio->status() != mobsya::ThymioNode::Status::Disconnected)
+            tabBar()->setTabText(idx, thymio->name());
     }
 }
 
@@ -83,63 +80,38 @@ void NodeTabsManager::setExecutionMode(int index, Target::ExecutionMode state) {
     }
 }
 
-void NodeTabsManager::removeAndDeleteTab(int index) {
-    if(index < 0) {
-        auto* button(qobject_cast<QWidget*>(sender()));
-        for(int i = 0; i < count(); ++i) {
-            if(tabBar()->tabButton(i, QTabBar::RightSide) == button) {
-                index = i;
-                break;
-            }
-        }
-    }
-    if(index >= 0) {
-        QWidget* w(widget(index));
-        QTabWidget::removeTab(index);
-        w->deleteLater();
-    }
-}
-
-void NodeTabsManager::vmMemoryResized(int col, int oldSize, int newSize) {
-    // keep track of the current value, to apply it on the other tabs
-    vmMemorySize[col] = newSize;
-}
-
 void NodeTabsManager::tabChanged(int index) {
     // resize the vmMemoryView, to match the user choice
     auto* tab = dynamic_cast<NodeTab*>(currentWidget());
     if(!tab)
         return;
 
-    vmMemoryViewResize(tab);
     // reset the tab highlight
     resetHighlight(index);
 }
 
+void NodeTabsManager::tabInserted(int) {
+    QTabWidget::setTabsClosable(count() > 1);
+}
+void NodeTabsManager::tabRemoved(int) {
+    QTabWidget::setTabsClosable(count() > 1);
+}
+
+void NodeTabsManager::onTabClosed(int index) {
+    auto t = qobject_cast<NodeTab*>(QTabWidget::widget(index));
+    if(!t)
+        return;
+    for(auto it = m_tabs.begin(); it != m_tabs.end(); ++it) {
+        if(it.value() == t) {
+            QTabWidget::removeTab(index);
+            m_tabs.erase(it);
+            return;
+        }
+    }
+}
+
 void NodeTabsManager::resetHighlight(int index) {
     tabBar()->setTabTextColor(index, Qt::black);
-}
-
-void NodeTabsManager::vmMemoryViewResize(NodeTab* tab) {
-    if(!tab)
-        return;
-
-    // resize the vmMemoryView QTreeView, according to the global value
-    for(int i = 0; i < 2; i++)
-        if(vmMemorySize[i] != -1)
-            tab->vmVariablesView->header()->resizeSection(i, vmMemorySize[i]);
-}
-
-void NodeTabsManager::readSettings() {
-    QSettings settings;
-    vmMemorySize[0] = settings.value(QStringLiteral("vmMemoryView/col0"), -1).toInt();
-    vmMemorySize[1] = settings.value(QStringLiteral("vmMemoryView/col1"), -1).toInt();
-}
-
-void NodeTabsManager::writeSettings() {
-    QSettings settings;
-    settings.setValue(QStringLiteral("vmMemoryView/col0"), vmMemorySize[0]);
-    settings.setValue(QStringLiteral("vmMemoryView/col1"), vmMemorySize[1]);
 }
 
 }  // namespace Aseba
--- aseba/clients/studio/NodeTabsManager.h
+++ aseba/clients/studio/NodeTabsManager.h
@@ -22,22 +22,18 @@ public Q_SLOTS:
     void onNodeRemoved(std::shared_ptr<mobsya::ThymioNode>);
     void onNodeModified(std::shared_ptr<mobsya::ThymioNode>);
 
-    void removeAndDeleteTab(int index = -1);
-
 protected slots:
-    void vmMemoryResized(int, int, int);  // for vmMemoryView QTreeView child widget
     void tabChanged(int index);
+    void onTabClosed(int index);
 
 protected:
-    virtual void resetHighlight(int index);
-    virtual void vmMemoryViewResize(NodeTab* tab);
-    virtual void readSettings();
-    virtual void writeSettings();
+    void resetHighlight(int index);
+    void tabInserted(int index) override;
+    void tabRemoved(int index) override;
 
 private:
     const mobsya::ThymioDeviceManagerClient& m_client;
     QMap<QUuid, NodeTab*> m_tabs;
-    int vmMemorySize[2];
 };
 
 
