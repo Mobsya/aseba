fb2b5c04c66f93dea9cdbc7e79c990fe3d444ee2
Corentin Jabot <corentin.jabot@mobsya.org>
2019-02-08 03:00:51 +0100

    [TDM] Connection to thymios through tty devices on Linux

--- aseba/thymio-device-manager/serial_usb_device.h
+++ aseba/thymio-device-manager/serial_usb_device.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <boost/asio/serial_port.hpp>
 #include "usb_utils.h"
-
+#ifdef __linux__
+#    include <sys/ioctl.h>
+#endif
 namespace mobsya {
 class usb_serial_port : public boost::asio::serial_port {
 public:
@@ -12,22 +14,33 @@ public:
     std::string device_name() const {
         return m_device_name;
     }
-
     void open() {
-        boost::asio::serial_port::open(m_port_name);
+        boost::system::error_code ec;
+        open(ec);
     }
-    void open(boost::system::error_code & ec) {
+    void open(boost::system::error_code& ec) {
         boost::asio::serial_port::open(m_port_name, ec);
+#ifdef __linux__
+        ioctl(native_handle(), TIOCEXCL);
+#endif
     }
 
     void set_data_terminal_ready(bool dtr) {
 #ifdef _WIN32
         EscapeCommFunction(native_handle(), dtr ? SETDTR : CLRDTR);
 #endif
+#ifdef __linux__
+        int flag = TIOCM_DTR;
+        ioctl(native_handle(), dtr ? TIOCMBIS : TIOCMBIC, &flag);
+#endif
     }
     void set_rts(bool rts) {
 #ifdef _WIN32
         EscapeCommFunction(native_handle(), rts ? SETRTS : CLRRTS);
+#endif
+#ifdef __linux__
+        int flag = TIOCM_RTS;
+        ioctl(native_handle(), rts ? TIOCMBIS : TIOCMBIC, &flag);
 #endif
     }
 
