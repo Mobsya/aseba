57cd85f213670667f793d66f24219a67192abbd3
Corentin Jabot <corentin.jabot@mobsya.org>
2019-05-02 09:51:28 +0200

    Fix trads identifiers

---
+++ aseba/launcher/src/translations/launcher_el.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el" sourcelanguage="en">
+<context>
+    <name>ApplicationSelectionView</name>
+    <message>
+        <location filename="../qml/ApplicationSelectionView.qml" line="78"/>
+        <source>&lt;a href=&quot;https://thymio.org&quot;&gt;Thymio Suite&lt;/a&gt; - %1 &lt;b&gt;BETA&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirmwareUpdateDialog</name>
+    <message>
+        <location filename="../qml/FirmwareUpdateDialog.qml" line="10"/>
+        <source>Update Firmware?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/FirmwareUpdateDialog.qml" line="11"/>
+        <source>Install Firmware %1 on %2?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/FirmwareUpdateDialog.qml" line="13"/>
+        <source>This should only take a few seconds.
+Do not unplug the device while the update is in progress.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="97"/>
+        <source>Thymio Suite</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsMenu</name>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="10"/>
+        <source>Launch a Simulator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="41"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThymioSelectionDeviceDelegate</name>
+    <message>
+        <location filename="../qml/ThymioSelectionDeviceDelegate.qml" line="134"/>
+        <source>%1 on this computer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionDeviceDelegate.qml" line="136"/>
+        <source>%1 on %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionDeviceDelegate.qml" line="140"/>
+        <source>This device cannot be selected because it is in a group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionDeviceDelegate.qml" line="143"/>
+        <source>This device cannot be selected because it is already being used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionDeviceDelegate.qml" line="225"/>
+        <source>A new firmware is available!
+Click to install it</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionDeviceDelegate.qml" line="226"/>
+        <source>A new firmware is available!
+Connect the robot with an usb cable to install it</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThymioSelectionView</name>
+    <message>
+        <location filename="../qml/ThymioSelectionView.qml" line="162"/>
+        <source>Choose a Thymio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionView.qml" line="187"/>
+        <source>Launch %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionView.qml" line="241"/>
+        <source>Connect a Thymio or &lt;a href=&apos;#&apos;&gt;launch a simulator&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionView.qml" line="257"/>
+        <source>No robot found because the Avahi Daemon is missing or not running. &lt;a href=&apos;http://google.com&apos;&gt;Troubleshooting&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ThymioSelectionView.qml" line="259"/>
+        <source>No robot found because the Bonjour service is missing or not running. &lt;a href=&apos;http://google.com&apos;&gt;Troubleshooting&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mobsya::Launcher</name>
+    <message>
+        <location filename="../launcher.cpp" line="130"/>
+        <source>Save %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
