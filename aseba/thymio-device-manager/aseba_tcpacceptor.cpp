12f4caea1dbf5e26ac849d0be0c8e576283f05e0
Corentin Jabot <corentin.jabot@mobsya.org>
2019-05-10 17:13:11 +0200

    Improve reconnection of the tcp endpoint

--- aseba/thymio-device-manager/aseba_tcpacceptor.cpp
+++ aseba/thymio-device-manager/aseba_tcpacceptor.cpp
@@ -12,7 +12,12 @@ static const std::map<std::string, aseba_endpoint::endpoint_type> endpoint_type_
     {"Dummy Node", aseba_endpoint::endpoint_type::simulated_dummy_node}};
 
 aseba_tcp_acceptor::aseba_tcp_acceptor(boost::asio::io_context& io_context)
-    : m_iocontext(io_context), m_stopped(false), m_contact("aseba"), m_monitor(m_monitor_ctx) {
+    : boost::asio::detail::service_base<aseba_tcp_acceptor>(io_context)
+    , m_iocontext(io_context)
+    , m_stopped(false)
+    , m_contact("aseba")
+    , m_monitor(m_monitor_ctx)
+    , m_active_timer(io_context) {
 
     m_monitor_thread = std::thread(boost::bind(&aseba_tcp_acceptor::monitor, this));
 }
@@ -21,6 +26,15 @@ aseba_tcp_acceptor::~aseba_tcp_acceptor() {
     m_stopped.store(true);
     m_monitor_ctx.stop();
     m_monitor_thread.join();
+}
+
+void aseba_tcp_acceptor::free_endpoint(aseba_endpoint* ep) {
+    auto it = m_known_contacts.find(ep);
+    if(it != m_known_contacts.end()) {
+        m_disconnected_contacts.insert(it->second);
+        m_known_contacts.erase(it);
+        boost::asio::post(m_iocontext, boost::bind(&aseba_tcp_acceptor::do_accept, this));
+    }
 }
 
 void aseba_tcp_acceptor::monitor() {
@@ -54,21 +68,35 @@ std::string remove_host_from_name(const std::string& str) {
 void aseba_tcp_acceptor::do_accept() {
     std::unique_lock<std::mutex> queue_mutex_lock(m_queue_mutex);
     if(m_pending_contacts.empty()) {
+        if(!m_disconnected_contacts.empty()) {
+            m_active_timer.expires_from_now(boost::posix_time::seconds(1));
+            m_active_timer.async_wait([this](auto ec) {
+                std::copy(m_disconnected_contacts.begin(), m_disconnected_contacts.end(),
+                          std::back_inserter(m_pending_contacts));
+                boost::asio::post(m_iocontext, boost::bind(&aseba_tcp_acceptor::do_accept, this));
+            });
+        }
+    }
+    if(m_pending_contacts.empty()) {
         return;
     }
-    auto session = aseba_endpoint::create_for_tcp(m_iocontext);
     aware::contact contact = m_pending_contacts.front();
-    m_pending_contacts.pop();
+    m_pending_contacts.erase(m_pending_contacts.begin());
     queue_mutex_lock.unlock();
+    do_accept_contact(contact);
+}
 
+
+void aseba_tcp_acceptor::do_accept_contact(aware::contact contact) {
+    auto session = aseba_endpoint::create_for_tcp(m_iocontext);
     if(contact.empty() || contact.type() != "aseba") {
         boost::asio::post(m_iocontext, boost::bind(&aseba_tcp_acceptor::do_accept, this));
         return;
     }
 
-    if(!mobsya::endpoint_is_local(contact.endpoint())) {
-        mLogTrace("Ignoring remote endoint {} (expected: {})", contact.endpoint().address().to_string(),
-                  boost::asio::ip::host_name());
+    if(!mobsya::endpoint_is_local(contact.endpoint()) && contact.name().find("Not A Thymio 3") != 0) {
+        mLogTrace("Ignoring remote endoint {} ({}) (expected: {})", contact.name(),
+                  contact.endpoint().address().to_string(), boost::asio::ip::host_name());
         boost::asio::post(m_iocontext, boost::bind(&aseba_tcp_acceptor::do_accept, this));
         return;
     }
@@ -84,6 +112,7 @@ void aseba_tcp_acceptor::do_accept() {
         }
     }
 
+    m_known_contacts.emplace(session.get(), contact);
     session->tcp().async_connect(
         contact.endpoint(), [this, contact = std::move(contact), session, key = key](boost::system::error_code ec) {
             const auto& properties = contact.properties();
@@ -91,10 +120,21 @@ void aseba_tcp_acceptor::do_accept() {
             aseba_endpoint::endpoint_type type = aseba_endpoint::endpoint_type::unknown;
             std::string type_str;
             if(ec) {
-                mLogWarn("[tcp] Fail to connect to aseba node {} {} : {}", contact.name(), contact.endpoint().address().to_string(), ec.message());
+                mLogWarn("[tcp] Fail to connect to aseba node {} {} : {}", contact.name(),
+                         contact.endpoint().address().to_string(), ec.message());
                 boost::asio::post(m_iocontext, [this] { this->accept(); });
                 return;
             }
+
+
+            // remove the contact from the list of disconnected contact
+            {
+                auto it = std::find_if(m_disconnected_contacts.begin(), m_disconnected_contacts.end(),
+                                       [contact](auto& other) { return contact == other; });
+                if(it != m_disconnected_contacts.end())
+                    m_disconnected_contacts.erase(it);
+            }
+
             {
                 std::unique_lock<std::mutex> _(m_endpoints_mutex);
                 auto it = m_connected_endpoints.find(key);
@@ -118,16 +158,17 @@ void aseba_tcp_acceptor::do_accept() {
             it = properties.find("protovers");
             if(it != std::end(properties))
                 protocol_version = std::atoi(it->second.c_str());
+
             session->set_endpoint_name(remove_host_from_name(contact.name()));
             session->set_endpoint_type(type);
             session->start();
             boost::asio::post(m_iocontext, [this] { this->accept(); });
         });
-}
+}  // namespace mobsya
 
 void aseba_tcp_acceptor::push_contact(aware::contact contact) {
     std::unique_lock<std::mutex> _(m_queue_mutex);
-    m_pending_contacts.push(std::move(contact));
+    m_pending_contacts.push_back(std::move(contact));
 }
 
 
