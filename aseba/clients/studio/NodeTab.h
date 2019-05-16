2b2c55c0b35e2929252131fe5b19a18246a022d3
Corentin Jabot <corentin.jabot@mobsya.org>
2019-05-16 08:55:53 +0200

    Properly save aesl when nodes are disconnected

--- aseba/clients/studio/NodeTab.h
+++ aseba/clients/studio/NodeTab.h
@@ -50,6 +50,9 @@ public:
 
     void clearEverything();
 
+    QVector<mobsya::EventDescription> eventsDescriptions() const;
+    mobsya::ThymioNode::VariableMap groupVariables() const;
+
 Q_SIGNALS:
     void uploadReadynessChanged(bool);
     void compilationSucceed();
@@ -164,10 +167,12 @@ private:
     ConstantsModel m_constants_model;
     ConstantsWidget* m_constantsWidget;
 
-
     MaskableVariablesModel m_events_model;
     EventsWidget* m_eventsWidget;
 
+
+    mobsya::ThymioNode::VariableMap m_group_variables;
+    QVector<mobsya::EventDescription> m_events_descriptions;
 
     QLabel* cursorPosText;
     QLabel* compilationResultImage;
