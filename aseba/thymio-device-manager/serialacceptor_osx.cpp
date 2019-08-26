fb27051348c4dccd5497d877c146e37605e3c7b5
Corentin Jabot <corentin.jabot@mobsya.org>
2019-08-26 09:31:46 +0200

    Wireless Configuration on osx

--- aseba/thymio-device-manager/serialacceptor_osx.cpp
+++ aseba/thymio-device-manager/serialacceptor_osx.cpp
@@ -48,15 +48,14 @@ void serial_acceptor_service::on_active_timer(const boost::system::error_code& e
     std::unique_lock<std::mutex> lock(m_req_mutex);
     this->handle_request_by_active_enumeration();
     if(!m_requests.empty()) {
-        m_active_timer.expires_from_now(boost::posix_time::seconds(1));  // :(
+        m_active_timer.expires_from_now(boost::posix_time::milliseconds(500));  // :(
         m_active_timer.async_wait(boost::asio::bind_executor(
             m_strand, boost::bind(&serial_acceptor_service::on_active_timer, this, boost::placeholders::_1)));
     }
 }
 
 
-static uint16_t property_as_short(io_registry_entry_t ioRegistryEntry,
-                                      const char* propertyKey) {
+static uint16_t property_as_short(io_registry_entry_t ioRegistryEntry, const char* propertyKey) {
     auto k = CFStringCreateWithCString(kCFAllocatorDefault, propertyKey, kCFStringEncodingUTF8);
     auto idx = IORegistryEntrySearchCFProperty(ioRegistryEntry, kIOServicePlane, k, kCFAllocatorDefault, 0);
     BOOST_SCOPE_EXIT(&k, &idx) {
@@ -72,8 +71,7 @@ static uint16_t property_as_short(io_registry_entry_t ioRegistryEntry,
     return value;
 }
 
-static std::string property_as_string(io_registry_entry_t ioRegistryEntry,
-                              const char* propertyKey) {
+static std::string property_as_string(io_registry_entry_t ioRegistryEntry, const char* propertyKey) {
 
     // get path for device
     auto k = CFStringCreateWithCString(kCFAllocatorDefault, propertyKey, kCFStringEncodingUTF8);
@@ -87,15 +85,12 @@ static std::string property_as_string(io_registry_entry_t ioRegistryEntry,
     if(!cfstr || CFGetTypeID(cfstr) != CFStringGetTypeID()) {
         return {};
     }
-    std::string str(CFStringGetCStringPtr(CFStringRef(cfstr),kCFStringEncodingUTF8));
-    str.erase(std::find_if(str.rbegin(), str.rend(), [](int ch) {
-        return !std::isspace(ch);
-    }).base(), str.end());
+    std::string str(CFStringGetCStringPtr(CFStringRef(cfstr), kCFStringEncodingUTF8));
+    str.erase(std::find_if(str.rbegin(), str.rend(), [](int ch) { return !std::isspace(ch); }).base(), str.end());
     return str;
 }
 
-static io_registry_entry_t get_parent(io_registry_entry_t service)
-{
+static io_registry_entry_t get_parent(io_registry_entry_t service) {
     io_registry_entry_t result = 0;
     ::IORegistryEntryGetParentEntry(service, kIOServicePlane, &result);
     ::IOObjectRelease(service);
@@ -108,14 +103,14 @@ void serial_acceptor_service::handle_request_by_active_enumeration() {
 
 
     CFMutableDictionaryRef classesToMatch = IOServiceMatching(kIOSerialBSDServiceValue);
-    if (classesToMatch == NULL)
+    if(classesToMatch == NULL)
         return;
     // specify all types of serial devices
     CFDictionarySetValue(classesToMatch, CFSTR(kIOSerialBSDTypeKey), CFSTR(kIOSerialBSDAllTypes));
     // get an iterator to serial port services
     io_iterator_t matchingServices;
     kern_return_t kernResult = IOServiceGetMatchingServices(kIOMasterPortDefault, classesToMatch, &matchingServices);
-    if (KERN_SUCCESS != kernResult) {
+    if(KERN_SUCCESS != kernResult) {
         mLogError("IOServiceGetMatchingServices failed");
         return;
     }
@@ -125,16 +120,16 @@ void serial_acceptor_service::handle_request_by_active_enumeration() {
     BOOST_SCOPE_EXIT_END
 
     std::vector<std::string> known_devices;
+    const auto devices = m_requests.front().acceptor.compatible_devices();
+
 
     // iterate over services
     io_object_t service;
-    while ((service = IOIteratorNext(matchingServices)))
-    {
+    while((service = IOIteratorNext(matchingServices))) {
         BOOST_SCOPE_EXIT(&service) {
             IOObjectRelease(service);
         }
         BOOST_SCOPE_EXIT_END
-        auto& req = m_requests.front();
         usb_device_identifier id{0, 0};
         std::string name, path;
         do {
@@ -146,12 +141,11 @@ void serial_acceptor_service::handle_request_by_active_enumeration() {
                 name = property_as_string(service, kUSBProductString);
             if(path.empty())
                 path = property_as_string(service, kIOCalloutDeviceKey);
-        }while((id.product_id == 0 || id.vendor_id == 0 || name.empty() || path.empty()) && (service = get_parent(service)));
-
-        const auto& devices = req.acceptor.compatible_devices();
+        } while((id.product_id == 0 || id.vendor_id == 0 || name.empty() || path.empty()) &&
+                (service = get_parent(service)));
 
         if(std::find(std::begin(devices), std::end(devices), id) == std::end(devices)) {
-            //mLogTrace("device {} not compatible : {:#06X}-{:#06X} ", name, id.vendor_id, id.product_id);
+            // mLogTrace("device {} not compatible : {:#06X}-{:#06X} ", name, id.vendor_id, id.product_id);
             continue;
         }
 
@@ -159,6 +153,12 @@ void serial_acceptor_service::handle_request_by_active_enumeration() {
             continue;
 
         known_devices.push_back(path);
+
+        if(m_requests.empty())
+            continue;
+
+
+        auto& req = m_requests.front();
         if(std::find(m_known_devices.begin(), m_known_devices.end(), path) != m_known_devices.end())
             continue;
 
@@ -173,7 +173,13 @@ void serial_acceptor_service::handle_request_by_active_enumeration() {
             const auto executor = boost::asio::get_associated_executor(handler, req.acceptor.get_executor());
             m_requests.pop();
             boost::asio::post(executor, boost::beast::bind_handler(handler, boost::system::error_code{}));
-            break;
+            continue;
+        }
+    }
+    for(auto&& e : m_known_devices) {
+        if(std::find(known_devices.begin(), known_devices.end(), e) == known_devices.end()) {
+            mLogError("Removed {}", e);
+            device_unplugged(e);
         }
     }
     m_known_devices = known_devices;
