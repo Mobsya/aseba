46207d0a67c34bc608e3aa3c2b4fda81bfbf741d
Michael Bonani <michael.bonani@mobsya.org>
2019-09-24 16:14:46 +0200

    translations (#649) fr it de pl
    * translations fr for launcher
    * Italian translation
    * fix link to bonjour error
    * polish translations
    * de translation
    * fix manually the Bonjour link in the translations
    * add polish description of apps

--- aseba/common/about/qtabout_pl.ts
+++ aseba/common/about/qtabout_pl.ts
@@ -4,89 +4,88 @@
 <context>
     <name>Aseba::AboutBox</name>
     <message>
-        <location filename="AboutDialog.cpp" line="77"/>
+        <location filename="AboutDialog.cpp" line="72"/>
         <source>Version %1</source>
         <translation>Wersja %1</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="88"/>
+        <location filename="AboutDialog.cpp" line="83"/>
         <source>You can find more information online at &lt;a href=&quot;%1&quot;&gt;%2&lt;/a&gt;.</source>
         <translation>Więcej informacji można znaleźć na stronie internetowej &lt;a href=&quot;%1&quot;&gt;%2&lt;/a&gt;.</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="95"/>
+        <location filename="AboutDialog.cpp" line="90"/>
         <source>This program is part of Aseba, a set of tools which allow beginners to program robots easily and efficiently. For more information about Aseba, visit %1.</source>
         <translation>Program ten jest częścią Aseby,który jest  zestawem narzędzi, umożliwiającym początkującym w łatwy i efektywny sposób programować roboty. Aby uzyskać więcej informacji na temat Aseby, odwiedź %1.</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="99"/>
+        <location filename="AboutDialog.cpp" line="94"/>
         <source>(c) 2006-2017 &lt;a href=&quot;http://stephane.magnenat.net&quot;&gt;Stéphane Magnenat&lt;/a&gt; and &lt;a href=&quot;https://github.com/mobsya/aseba/blob/master/authors.txt&quot;&gt;other contributors&lt;/a&gt;. See tabs &quot;Authors&quot; and &quot;Thanks To&quot; for more information.</source>
         <translation>(c) 2006-2017 &lt;a href=&quot;http://stephane.magnenat.net&quot;&gt;Stéphane Magnenat&lt;/a&gt; i &lt;a href=&quot;https://github.com/mobsya/aseba/blob/master/authors.txt&quot;&gt;inne podmioty wnoszące wkład&lt;/a&gt;. Więcej informacji można znaleźć w zakładkach &quot;Autorzy&quot; i &quot;Podziękowania&quot;.</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="103"/>
+        <location filename="AboutDialog.cpp" line="98"/>
         <source>Aseba is open-source licensed under the &lt;a href=&quot;https://www.gnu.org/licenses/lgpl.html&quot;&gt;LGPL version 3&lt;/a&gt;.</source>
         <translation>Aseba posiada licencję open-source na podstawie &lt;a href=&quot;https://www.gnu.org/licenses/lgpl.html&quot;&gt;LGPL wersja 3&lt;/a&gt;.</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="113"/>
+        <location filename="AboutDialog.cpp" line="108"/>
         <source>About</source>
-        <translation>Na temat</translation>
+        <translation>O programie</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="122"/>
+        <location filename="AboutDialog.cpp" line="117"/>
         <source>Usage</source>
         <translation>Zastosowanie</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="127"/>
+        <location filename="AboutDialog.cpp" line="122"/>
         <source>Authors</source>
         <translation>Autorzy</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="127"/>
+        <location filename="AboutDialog.cpp" line="122"/>
         <source>Thanks To</source>
-        <translation>Podziękowania</translation>
+        <translation>Podziękowania dla</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="180"/>
+        <location filename="AboutDialog.cpp" line="175"/>
         <source>&lt;b&gt;%1&lt;/b&gt; grant %2&lt;br/&gt;%3</source>
         <translation>&lt;b&gt;%1&lt;/b&gt; dofinansowanie %2&lt;br/&gt;%3</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="201"/>
+        <location filename="AboutDialog.cpp" line="196"/>
         <source>The following software are used in Aseba:</source>
         <translation>W Aseba używane jest następujące oprogramowanie:</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="202"/>
+        <location filename="AboutDialog.cpp" line="197"/>
         <source>&lt;b&gt;&lt;a href=&quot;%3&quot;&gt;%1&lt;/a&gt;&lt;/b&gt; version %2</source>
         <translation>&lt;b&gt;&lt;a href=&quot;%3&quot;&gt;%1&lt;/a&gt;&lt;/b&gt; wersja %2</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="204"/>
+        <location filename="AboutDialog.cpp" line="199"/>
         <source>build version %1, protocol version %2</source>
-        <translation>wersja budowy %1, wersja protokołu %2</translation>
+        <translation>wersja %1, wersja protokołu %2</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="206"/>
         <source>supported stream types: %1</source>
-        <translation>obsługiwane rodzaje strumieni: %1</translation>
+        <translation type="vanished">obsługiwane rodzaje strumieni: %1</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="228"/>
+        <location filename="AboutDialog.cpp" line="216"/>
         <source>Libraries</source>
-        <translation>Biblioteka</translation>
+        <translation>Biblioteki</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="233"/>
+        <location filename="AboutDialog.cpp" line="221"/>
         <source>close</source>
-        <translation>zamknięcie</translation>
+        <translation>Zamknij</translation>
     </message>
     <message>
-        <location filename="AboutDialog.cpp" line="246"/>
+        <location filename="AboutDialog.cpp" line="234"/>
         <source>About %1</source>
-        <translation>Na temat %1</translation>
+        <translation>O %1</translation>
     </message>
 </context>
 </TS>
