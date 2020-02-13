e3bf7e2b69f382fe656fd7f3cfbda174fa74b1fd
Davide Poveromo <davidepoveromolavoro@gmail.com>
2020-02-13 12:25:29 +0100

    remove sliding window logic and introduced reset button

--- aseba/clients/studio/PlotTab.cpp
+++ aseba/clients/studio/PlotTab.cpp
@@ -1,16 +1,31 @@
 #include "PlotTab.h"
 #include <QtCharts>
 #include <QVBoxLayout>
+#include <QPushButton>
 
 namespace Aseba {
 
 PlotTab::PlotTab(QWidget* parent) : QWidget(parent) {
 
     auto layout = new QVBoxLayout;
+    auto topButtonsLayout = new QHBoxLayout;
+
+    reloadButton = new QPushButton();
+    reloadButton->setIcon(QIcon(":/images/rescan.png"));
+    reloadButton->setToolTip(tr("Reload"));
+    reloadButton->setText(tr("Reload"));
+    reloadButton->setStyleSheet("QPushButton:pressed { border: 1px solid blue }");
+
+    spacer = new QSpacerItem(1, 1, QSizePolicy::Expanding);
+
+    topButtonsLayout->addWidget(reloadButton);
+    topButtonsLayout->addSpacerItem(spacer);
+    
     m_chart = new QtCharts::QChart();
     QChartView* chartView = new QChartView(m_chart, this);
     chartView->setRubberBand(QChartView::VerticalRubberBand);
     chartView->setRenderHint(QPainter::Antialiasing);
+    layout->addLayout(topButtonsLayout);
     layout->addWidget(chartView);
     this->setLayout(layout);
 
@@ -23,6 +38,8 @@ PlotTab::PlotTab(QWidget* parent) : QWidget(parent) {
     m_yAxis->setLabelFormat("%i");
     m_yAxis->setTitleText(tr("Values"));
     m_chart->addAxis(m_yAxis, Qt::AlignLeft);
+
+    connect(reloadButton, SIGNAL(clicked()), this, SLOT(clearData()));
 
     /*QTimer* t = new QTimer(this);
     connect(t, &QTimer::timeout, this, [this]() { m_chart->scroll(-5, 0); });
@@ -73,10 +90,8 @@ void PlotTab::onEvents(const mobsya::ThymioNode::EventMap& events, const QDateTi
         const QVariant& v = it.value();
         plot(event.key(), v, event.value(), timestamp);
     }
-    // previous 2000 data point ( 1 every 10 milliseconds) = 20 seconds window
-    const int nPreviousData = 2000;
+
     m_xAxis->setMax(m_start.msecsTo(timestamp) + 10);
-    m_xAxis->setMin(m_start.msecsTo(timestamp) - 10 * nPreviousData);
 }
 
 void PlotTab::onVariablesChanged(const mobsya::ThymioNode::VariableMap& vars, const QDateTime& timestamp) {
@@ -87,10 +102,28 @@ void PlotTab::onVariablesChanged(const mobsya::ThymioNode::VariableMap& vars, co
         const QVariant& v = it.value().value();
         plot(event.key(), v, event.value(), timestamp);
     }
-    // previous 2000 data point ( 1 every 10 milliseconds) = 20 seconds window
-    const int nPreviousData = 2000;
+    
     m_xAxis->setMax(m_start.msecsTo(timestamp) + 10);
-    m_xAxis->setMin(m_start.msecsTo(timestamp) - 10 * nPreviousData);
+}
+
+void PlotTab::clearData( ){
+    for(auto it = m_events.begin(); it != m_events.end(); ++it) {
+        auto series = it.value();
+        for(auto it_s = series.begin(); it_s != series.end(); ++it_s) {
+            auto serie = *it_s;
+            serie->clear();
+        }
+    }
+    for(auto it = m_variables.begin(); it != m_variables.end(); ++it) {
+        auto series = it.value();
+        for(auto it_s = series.begin(); it_s != series.end(); ++it_s) {
+            auto serie = *it_s;
+            serie->clear();
+        }
+    }
+
+    m_start = QDateTime::currentDateTime();
+    m_xAxis->setMin(0);
 }
 
 
