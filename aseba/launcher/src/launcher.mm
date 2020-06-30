b44a001a66de18ff1093633c2af62e1c18c886e3
Davide Poveromo <davidepoveromolavoro@gmail.com>
2020-07-01 01:33:29 +0200

    forcing .vpl3 format to .json

--- aseba/launcher/src/launcher.mm
+++ aseba/launcher/src/launcher.mm
@@ -116,7 +116,9 @@ bool isScratch(NSString *URL){
         //First save it in the app with a real name
         // create url
         NSURL *url;
-        if([name containsString:@".vpl3"] || [name containsString:@".html"] ){
+        if([name containsString:@".vpl3"]){
+            url = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingString:[name stringByAppendingString:@".json"]]];
+        } else if([name containsString:@".html"] ){
             url = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingString:name]];
         } else {
             url = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingString:[name stringByAppendingString:@".zip"]]];
